2674dff77ea8921aab23716e09ab78d60f3c1a19
Oki Kehinde <olamiposi.oki@gmail.com>
2021-09-15 01:37:09 +0100

    set up custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			black: {
+				DEFAULT: '#181719',
+			},
+		},
+		extend: {},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
